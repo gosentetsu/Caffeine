6f2680ef4a987b1b63e883ddd2f0c4933b328a98
xianzhewu <xianzhewu@tencent.com>
2026-06-23 16:13:34 +0800

    Initial Commit

---
+++ caffeine/caffeineApp.swift
@@ -0,0 +1,17 @@
+//
+//  caffeineApp.swift
+//  caffeine
+//
+//  Created by gosentetsu on 2026/6/23.
+//
+
+import SwiftUI
+
+@main
+struct caffeineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
